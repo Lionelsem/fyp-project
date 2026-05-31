0a8a7b2925e64d762999c9f5e4626cb705f88c18
Lionelsem <24045619@myrp.edu.sg>
2026-05-31 22:49:15 +0800

    update-adding-user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Auth } from './components/auth';
+import {db} from'./config/firebase';
+import {getDocs, collection} from 'firebase/firestore';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [user,setUser] = useState([]);
+
+  const userCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const data = await getDocs(userCollectionRef);
+        setUser(data.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getUser();
+  }, []);
+
   return (
     <div className="App">
       <Auth />
