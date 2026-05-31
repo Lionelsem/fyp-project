038f369b26e00e7177e9dbbe4d8bcd0f56fa0b7b
SitiRaudhah123 <24042911@myrp.edu.sg>
2026-05-31 19:35:37 +0800

    Add in Firestore and Users

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { auth } from '../config/firebase';
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 
 export const Auth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [userData, setUserData] = useState(null);
 
     const signIn = async () => {
         try {
             await createUserWithEmailAndPassword(auth, email, password);
+
+            const user = userCredential.user;
+
+      await setDoc(doc(db, "users", user.uid), {
+        fullName: "Test User",
+        email: user.email,
+        phoneNumber: "91234567",
+        role: "Admin",
+        status: "Active",
+        createdAt: new Date()
+      });
+
+      const docSnap = await getDoc(doc(db, "users", user.uid));
+      if (docSnap.exists()) {
+        setUserData(docSnap.data());
+      }
         } catch (err) {
             console.error(err);
         }
@@ -17,6 +35,7 @@ export const Auth = () => {
         const logout = async () => {
         try {
             await signOut(auth);
+            setUserData(null);
         } catch (err) {
             console.error(err);
         }
@@ -38,7 +57,17 @@ export const Auth = () => {
             />
             <button onClick={signIn}>Sign In</button>
             <button onClick={logout}>Logout</button>
-
+            
+            {userData && (
+        <div>
+            <h3>Firestore User Data</h3>
+            <p>Full Name: {userData.fullName}</p>
+            <p>Email: {userData.email}</p>
+            <p>Phone: {userData.phoneNumber}</p>
+            <p>Role: {userData.role}</p>
+            <p>Status: {userData.status}</p>
+        </div>
+      )}
         </div>
     );
 };
